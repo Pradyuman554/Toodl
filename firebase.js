41915c321f628fcc043d747ea67dbd8ba68255fe
Pradyuman Sharma <pradyumanmusic@gmail.com>
2024-08-29 22:40:18 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,7 @@
+import {initializeApp} from 'firebase/app'; //Initialize App
+import {getAuth} from 'firebase/auth'   //Get authentication
+import {getFirestore} from 'firebase/firestore' //Get Database
+
+const firebaseConfig = {
+    apiKey : 
+}

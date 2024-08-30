8a96308e2340c8cab1d27a5dcb979113a099720b
Pradyuman Sharma <pradyumanmusic@gmail.com>
2024-08-30 15:46:26 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,35 +1,45 @@
 import { Fugaz_One, Open_Sans } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
+import Head from './head';
+import { AuthProvider } from '@/context/AuthContext';
 
 const sans = Open_Sans({ subsets: ["latin"] });
 const fugaz = Fugaz_One({ subsets: ["latin"], weight:['400'] });
 
 export const metadata = {
-  title: "Toodle",
+  title: "Toodl",
   description: "Toodle: Track your emotions",
 };
 
 export default function RootLayout({ children }) {
   const header = (
-    <header className="flex gap-4 p-4 sm:p-8 items-center justify-between text-lime-600">
-    <h1 className={' '+fugaz.className}>Toodle</h1>
-    <div className="flex items-center justify-between">PLACEHOLDER CTA || STATS</div>
+    <header className="flex gap-4 p-4 sm:p-8 items-center justify-between">
+      <Link>
+        <h1 className={'text-base sm:text-lg textGradient ' + fugaz.className}>Toodl</h1>
+      </Link>
+      <Logout/>
     </header>
   )
 
   const footer = (
     <footer className={'p-4 sm:p-8 flex justify-center ' + (fugaz.className)}>
-      <p className="text-lime-500">Made with 💚</p>
+      <Link href={'https://www.linkedin.com/in/pradyuman-5-sharma/'}>
+        <p className="text-lime-500">Made by Pradyuman💚</p>
+      </Link>
       </footer>
   ) 
 
   return (
     <html lang="en">
+      <Head/>
+      <AuthProvider>
       <body className={'w-full max-w-[1000px] min-h-screen mx-auto flex flex-col text-sm sm:text-base '+ sans.className}>
       {header}
       {children}
       {footer}
       </body>
+      </AuthProvider>
     </html>
   );
 }

30e7792745ede4d2283c89ae4c5364146793b798
Pradyuman Sharma <pradyumanmusic@gmail.com>
2024-08-30 18:01:45 +0530

    Update Dashboard.js

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,5 +1,15 @@
-import React from "react";
+'use client'
+import { Fugaz_One } from "next/font/google";
+import React, {useState, useEffect} from "react";
+import Calendar from "./Calendar";
+import { useAuth } from "../context/AuthContext";
+import { doc, setDoc } from "firebase/firestore";
+import { db } from "../firebase";
+import Loading from "./Loading";
+import Login from "./Login";
+
+const fugaz = Fugaz_One({ subsets: ["latin"], weight: ['400'] });
 
 export default function Dashboard(){
-  return <div>Dashboard</div>;
+  const {currentUser, userDataObj, setUserDataObj, loading} = useAuth()
 }

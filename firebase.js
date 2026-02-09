12e96c8c47ef3aeff212c6a25a4192afa4d41965
Pradyuman Sharma <pradyumanmusic@gmail.com>
2026-02-09 09:08:16 +0530

    remove console

--- firebase.js
+++ firebase.js
@@ -2,8 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-console.log(process.env.NEXT_PUBLIC_API_KEY)
-
 const firebaseConfig = {
   apiKey : process.env.NEXT_PUBLIC_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
@@ -17,4 +15,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
+
 export const db = getFirestore(app)
